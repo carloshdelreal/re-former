3873166c9d8671a947d81880a32ee4fe6fb74491
trox115 <tofernandes67@gmail.com>
2019-08-09 21:00:32 +0100

    added private to user params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,4 +11,15 @@ class UsersController < ApplicationController
           render :new
         end
     end
+
+
+
+
+
+
+    private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 end
