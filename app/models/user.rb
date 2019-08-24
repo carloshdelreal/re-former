ac11278169a35f4bbd90c5dfa1de22898e0e7d1f
Carlos Del Real <carloshdelreal@gmail.com>
2019-08-24 15:20:31 -0500

    Fix the comma separation in the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     
     validates :username, presence: true, uniqueness:  { case_sensitive: false }
-    validates :email, presence: true,format: { with: VALID_EMAIL_REGEX },uniqueness:  { case_sensitive: false }
+    validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness:  { case_sensitive: false }
     validates :password, presence: true, length: { minimum: 5 }
     
 
